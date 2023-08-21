195824bd05d67785d2820b79a8cbe7bed6612a24
Ana Ion <ana.marilena.ion@gmail.com>
2023-08-21 11:46:55 +0300

    Add button and select for projects

---
+++ www-public/assets/js/projects.js
@@ -0,0 +1,46 @@
+const projectList = [
+    {
+        title: "Something",
+        description: "This project does this and this",
+        preview: "somepage",
+        code: "someurl",
+        date: "2023-06-20",
+        highlighted: true,
+        specs: [
+            "React", "JavaScript"
+        ]
+    },
+    {
+        title: "Something",
+        description: "This project does this and this",
+        preview: "somepage",
+        code: "someurl",
+        date: "2022-06-20",
+        highlighted: false,
+        specs: [
+            "Java", "Maven"
+        ]
+    },
+    {
+        title: "Something",
+        description: "This project does this and this",
+        preview: "somepage",
+        code: "someurl",
+        date: "2022-06-20",
+        highlighted: false,
+        specs: [
+            "JavaScript", "Angular"
+        ]
+    },
+    {
+        title: "Something",
+        description: "This project does this and this",
+        preview: "somepage",
+        code: "someurl",
+        date: "2022-06-20",
+        highlighted: false,
+        specs: [
+            "JavaScript", "Angular"
+        ]
+    }
+]

06b31cd3e9c2b0e1e2fb0f7ace8981e89893a252
astrahalora <astrahalora@gmail.com>
2023-09-10 18:22:58 +0300

    Fix nav display

--- www-public/assets/js/general.js
+++ www-public/assets/js/general.js
@@ -1,21 +1,39 @@
 const navElement = document.querySelector(".main-nav");
 const hamburger = document.querySelector(".hamburger");
+let isNavVisible = true;
 
 function updateDisplay() {
-    if(window.innerWidth > 670) {
+    if (window.innerWidth > 670) {
         navElement.style.display = "flex";
+        isNavVisible = true;
     } else {
         navElement.style.display = "none";
-    }
+        isNavVisible = false;
+    };
 };
 
-hamburger.addEventListener("click", function() {
-    if(window.getComputedStyle(navElement).display === "none") {
-        navElement.style.display = "flex";
-    } else {
+hamburger.addEventListener("click", function () {
+    if (isNavVisible) {
         navElement.style.display = "none";
+        isNavVisible = false; 
+    } else {
+        navElement.style.display = "flex";
+        isNavVisible = true; 
     };
 });
 
+if (window.innerWidth <= 670) {
+    const pageLinks = Array.from(navElement.querySelectorAll("a"));
+    pageLinks.forEach((pageLink) => {
+        pageLink.addEventListener("click", function () {
+            navElement.style.display = "none";
+            isNavVisible = false; 
+        });
+    });
+};
+
 window.addEventListener("resize", updateDisplay);
+
+updateDisplay();
+
 

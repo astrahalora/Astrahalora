d65425795dce10c208b0736fc89ac04c53fdbe24
astrahalora <astrahalora@gmail.com>
2023-09-11 19:40:43 +0300

    Make pages responsive

---
+++ www-public/assets/js/about.js
@@ -0,0 +1,15 @@
+const footerElement = document.querySelector(".footer");
+
+function handleWindowResize() {
+    if (window.innerWidth <= 1000) {
+        footerElement.classList.remove("fixed-footer");
+    } else {
+        if (!footerElement.classList.contains("fixed-footer")) {
+            footerElement.classList.add("fixed-footer");
+        }
+    }
+}
+
+handleWindowResize();
+
+window.addEventListener("resize", handleWindowResize);
